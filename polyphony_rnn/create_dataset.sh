01d91740ebb72cc24e38385e4d09b05e762acd08
Alexandre Juppet <37328768+AlexandreJup@users.noreply.github.com>
2019-07-15 17:39:15 +0100

    1507 update

--- polyphony_rnn/create_dataset.sh
+++
@@ -1,4 +0,0 @@
-polyphony_rnn_create_dataset \
---input=/Note_sequences/notesequences.tfrecord \
---output_dir=/polyphony_rnn/sequence_examples \
---eval_ratio=0.20
--- polyphony_rnn/generate_seq.sh
+++
@@ -1,9 +0,0 @@
-polyphony_rnn_generate \
---run_dir=polyphony_rnn/logdir/run1 \
---hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
---output_dir=polyphony_rnn/generated \
---num_outputs=10 \
---num_steps=128 \
---primer_midi=/Midi_archives/irish_midi/BARNEY.MID
---condition_on_primer=false \
---inject_primer_during_generation=false
